2adf47aa9f7df986c2173215c9e2f4af2623ee1d
Hrishi Mittal <hrishimittal@gmail.com>
2024-09-12 07:37:27 +0100

    Add test for Project status changes

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'status changes' do
+    let(:project) { Project.create(status: Project.statuses.keys.first) }
+
+    it 'can change to valid statuses' do
+      Project.statuses.keys.each do |status|
+        expect(project.update(status: status)).to be true
+        expect(project.reload.status).to eq status
+      end
+    end
+
+    it 'cannot change to invalid status' do
+      expect { project.update!(status: 'Invalid Status') }.to raise_error(ArgumentError)
+      expect(project.reload.status).to eq Project.statuses.keys.first
+    end
+  end
 end
