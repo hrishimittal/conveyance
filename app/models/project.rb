f8a6899fd5a883a3fa85dd6db7c6dd8dd8f747a4
Hrishi Mittal <hrishimittal@gmail.com>
2024-09-12 07:25:30 +0100

    Add list of Project statuses

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :project_changes, dependent: :destroy
+
+  enum status: {
+    draft: "Draft",
+    planning: "In Planning",
+    review: "Under Review",
+    completed: "Completed",
+    archived: "Archived"
+  }, _prefix: true
 end
