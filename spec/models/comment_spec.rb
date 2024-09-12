88e9485eceb8d3e0b5b5ce4191f953ef9b16cfdc
Hrishi Mittal <hrishimittal@gmail.com>
2024-09-12 07:32:54 +0100

    Add spec for adding a comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'adding a comment on a project' do
+    let(:project) { Project.create(name: 'Test Project') }
+    let(:user) { User.create(name: 'Test User', email: 'test@example.com', password: 'password') }
+    let(:comment) { Comment.new(body: 'Test comment', project: project, user: user) }
+
+    it 'saves the comment' do
+      expect { comment.save }.to change(Comment, :count).by(1)
+    end
+  end
 end
